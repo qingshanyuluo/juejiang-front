a95c44de177ffc04fed4947703cdb2b067e2017d
lennon <qingshanyuluo@gmail.com>
2019-12-26 20:51:04 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
+    <van-tabbar route> 
+      <van-tabbar-item icon="home-o" to="/">标签</van-tabbar-item>
+      <van-tabbar-item icon="home-o" to="/explorer">标签</van-tabbar-item>
+      <van-tabbar-item icon="search" dot>标签</van-tabbar-item>
+      <van-tabbar-item icon="friends-o" info="5">标签</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" info="20">标签</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
+<script>
+import Vue from 'vue';
+import { Tabbar, TabbarItem } from 'vant';
+Vue.use(Tabbar).use(TabbarItem);
+export default {
+  components: {
+
+  }
+  
+}
+</script>
+
 <style>
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
